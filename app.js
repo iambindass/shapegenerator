51112f483dd6defefe3d913db783ae8d92cda32e
iambindass <sathwikvarma17@gmail.com>
2023-08-21 12:34:45 +0530

    updated

---
+++ app.js
@@ -0,0 +1,34 @@
+let button = document.getElementById("button");
+var numberInTheShape = 1;
+var oldvalue = 0;
+
+button.onclick = () => {
+    let n = document.getElementById('number').value;
+    const circle = document.getElementById("circle");
+    const square = document.getElementById("square");
+    const rectangle = document.getElementById("rectangle");
+    const $box = document.getElementById("box");
+    let j;
+
+    n = Number(oldvalue) + Number(n);
+
+    for(let j = numberInTheShape; j<=n; j++){
+        var shape = document.createElement("div")
+        shape.innerHTML += numberInTheShape;
+        if(square.checked){
+            shape.classList.add("square")
+        }
+        else if(circle.checked){
+            shape.classList.add("circle")
+        }
+        else if(rectangle.checked){
+            shape.classList.add("rectangle")
+        }
+    else{
+    document.write("invalid output");
+        }
+$box.appendChild(shape);
+numberInTheShape++;
+oldvalue = document.getElementById("box").lastElementChild.innerHTML;
+    }
+};
